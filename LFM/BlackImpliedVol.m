278971dbe7ae9491770fa410919ff39a1dcb3f4b
JamesLTaylor <jameslatimertaylor@gmail.com>
2015-10-12 08:30:47 +0200

    Added more line breaks to present on small screen

--- LFM/BlackImpliedVol.m
+++ LFM/BlackImpliedVol.m
@@ -1,3 +1,5 @@
 function v = BlackImpliedVol(F, K, T, multiplier, omega, price)
-fun = @(vol) (Black(F, K, vol, T, multiplier, omega) - price)^2;
+fun = @(vol)...
+    (Black(F, K, vol, T, multiplier, omega)...
+    - price)^2;
 v = fminbnd(fun,1e-6,1);
